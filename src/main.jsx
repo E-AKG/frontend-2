80b34477298a836743de91288e3d501d380cd0b5
E-AKG <eyluel2020@gmail.com>
2025-12-11 20:09:50 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AppProvider } from "./contexts/AppContext";
+import App from "./App.jsx";
+import { AppProvider } from "./contexts";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
